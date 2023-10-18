75675305021cd7d9f1df9da059eb2e3d1a965cdf
Yoann-CH <yoann.chambeux@edu.devinci.fr>
2023-10-18 14:10:19 +0200

    feat(create-puzzleify): create puzzleify's constructor

--- src/puzzleify.js
+++ src/puzzleify.js
@@ -1,3 +1,31 @@
 export class Puzzleify {
-    
+    constructor(imgElement, piecesX, piecesY, mode = 'default') {
+        this.imgElement = imgElement;
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = imgElement.width;
+        this.canvas.height = imgElement.height;
+        imgElement.parentNode.replaceChild(this.canvas, imgElement);
+        imgElement.style.display = 'none';
+        this.ctx = this.canvas.getContext('2d');
+        this.img = new Image();
+        this.img.src = imgElement.src;
+        this.piecesX = piecesX;
+        this.piecesY = piecesY;
+        this.pieceWidth = this.canvas.width / this.piecesX;
+        this.pieceHeight = this.canvas.height / this.piecesY;
+        this.pieces = [];
+        this.draggingPiece = null;
+        this.offsetX = 0;
+        this.offsetY = 0;
+        this.mode = mode;
+
+        this.img.onload = () => {
+            this.createPieces();
+            this.shufflePieces();
+            this.drawPuzzle();
+            this.addEventListeners();
+        };
+
+        this.imgElement.puzzleInstance = this;
+    }
 }
